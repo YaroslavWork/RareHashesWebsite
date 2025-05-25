c7ab8c5eabe49fd025a4741a54043d0fe8b876b5
YaroslavWork <zahorodnyi.yaroslav.work@gmail.com>
2025-05-25 22:16:55 +0200

    add unit tests to utils, add docstring to utils, polishing functions

--- app/utils/hash_utils.py
+++ app/utils/hash_utils.py
@@ -1,17 +1,52 @@
 import hashlib
 
 def count_repeated_pattern_from_start(hash: str) -> int:
+    """
+    Counts how many times the first character is repeated consecutively from the start of a binary hash string.
+
+    Args:
+        hash (str): A binary string representation of the hash.
+
+    Returns:
+        int: The count of repeated characters starting from the beginning. Returns 0 if the input string is empty.
+    """
+
+    if hash == "":
+        return 0
+
     sign: str = hash[0]
     for i in range(1, len(hash)):
         if hash[i] != sign:
             return i
+        
+    return len(hash)
 
 
 def get_sha256_hash(word: str) -> str:
+    """
+    Convert a string into a SHA-256 hash.
+
+    Args:
+        word (str): A input string to be hashed.
+
+    Returns:
+        str: The resulting SHA-256 hash as a hexadecimal string.
+    """
+
     return hashlib.sha256(word.encode()).hexdigest()
 
 
 def convert_hex_to_binary(hex: str) -> str:
-    hash_in_binary = format(int(hex, 16), '0>42b')
+    """
+    Convert a hexadecimal string into its 256-bit binary representation.
+
+    Args:
+        hex (str): A hexadecimal string.
+
+    Returns:
+        str: A 256-character binary string.
+    """
+
+    hash_in_binary: str = format(int(hex, 16), '0>42b')
     padding = "0" * (256-len(hash_in_binary))
     return f"{padding}{hash_in_binary}"
