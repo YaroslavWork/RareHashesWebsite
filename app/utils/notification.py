b1ad283f45cf8a658e0401ab6e04989c1dd6af67
YaroslavWork <zahorodnyi.yaroslav.work@gmail.com>
2025-06-03 02:27:06 +0200

    saving logs to the logs.txt (closes #27)

--- app/utils/notification.py
+++ app/utils/notification.py
@@ -1,16 +1,25 @@
 from datetime import datetime
 
-def log(title, message, with_timestamp: bool = True):
+def log(title: str, message: str, with_timestamp: bool = True, save_to_file: bool = True) -> None:
     """
     Logs for debugging
 
     Args:
-        title (_type_): Log title
-        message (_type_): Log description
+        title (str): Log title
+        message (str): Log description
         with_timestamp (bool, optional): With time mark or without. Defaults to True.
+        save_to_file (bool, optional): Logs is saving to file (logs.txt). Defaults to True.
     """
     
+    text = ""
     if with_timestamp:
         now = datetime.now()
         formatted = now.strftime("%d.%m.%Y %H:%M:%S.") + f"{now.microsecond:03d}"
-        print(f"({formatted}) {title}: {message}")
+        text = f"({formatted}) {title}: {message}"
+    else:
+        text = f"({title}: {message}"
+    print(text)
+
+    if save_to_file:
+        with open("./logs.txt", 'a') as file:
+            file.write(f'{text}\n')
